39dbd615865f72a04574867acdf5981c0ba7af2a
Ronald <kpn.nl.rs@gmail.com>
2022-05-20 11:43:20 +0200

    Add missing file

---
+++ Assets/Scripts/DictionaryViewAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Unity.Collections.LowLevel.Unsafe;
+using UnityEditor;
+using UnityEngine;
+
+public class DictionaryViewAttribute : PropertyAttribute
+{
+    
+}
+
+#if UNITY_EDITOR
+public class DictionaryViewDrawer : PropertyDrawer
+{
+    struct SendHelp { int a; int b; }
+    //unsafe void a()
+    //{
+    //    int a = 567;
+    //    int* b = &a;
+    //    void* c = (void*)b;
+    //    SendHelp* f = (SendHelp*)c;
+
+    //    int d = 2357;
+    //    void* e = (void*)&d;
+
+    //    UnsafeUtility.MemMove(c, e, sizeof(int));
+    //}
+}
+#endif
