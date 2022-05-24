fcf82225ff42fb97adf7a0b23a30a16e3b882df7
Ronald <kpn.nl.rs@gmail.com>
2022-05-24 09:21:26 +0200

    Add util to see scene file references (Now survives reload of editor)

---
+++ Assets/Scripts/Editor/SceneReferenceEditor.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class SceneReferenceEditor : EditorWindow
+{
+    public static string RegexExpression { get; }  = "([0-9a-f]){32}";
+    public static RegexOptions regexOptions { get; } = RegexOptions.Multiline;
+
+    public int editButtonSizeMod = 8;
+    public string ScorePath;
+
+    Vector2 scrollPos = Vector2.zero;
+    MatchCollection guids;
+    bool editMode = false;
+    bool reload = true;
+
+    [MenuItem("Tools/Experimental/View scene references")]
+    public static void OpenViewer()
+    {
+        SceneReferenceEditor window = GetWindow<SceneReferenceEditor>("Reference editor [View]");
+        window.ScorePath = EditorSceneManager.GetActiveScene().path;
+        window.guids = Regex.Matches(File.ReadAllText(window.ScorePath), RegexExpression);
+        window.reload = false;
+    }
+    [MenuItem("Tools/Experimental/Edit scene references")]
+    public static void OpenEditor()
+    {
+        SceneReferenceEditor window = GetWindow<SceneReferenceEditor>("Reference editor [Edit]");
+        window.ScorePath = EditorSceneManager.GetActiveScene().path;
+        window.guids = Regex.Matches(File.ReadAllText(window.ScorePath), RegexExpression, regexOptions);
+        window.editMode = true;
+        window.reload = false;
+    }
+
+    private void OnEnable()
+    {
+        reload = true;
+    }
+
+    private void OnGUI()
+    {
+        if (reload)
+        {
+            ScorePath = EditorSceneManager.GetActiveScene().path;
+            guids = Regex.Matches(File.ReadAllText(ScorePath), RegexExpression, regexOptions);
+            reload = false;
+        }
+        if (!editMode)
+        {
+            scrollPos = GUILayout.BeginScrollView(scrollPos);
+            foreach (Match xmatch in guids)
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.Label(new GUIContent(xmatch.Value), GUILayout.Width(184));
+            
+                GUILayout.Label(new GUIContent("->"), GUILayout.Width(32));
+                string assetPath = AssetDatabase.GUIDToAssetPath(xmatch.Value);
+
+                GUILayout.Label(new GUIContent(assetPath));
+                GUILayout.EndHorizontal();
+            }
+            GUILayout.EndScrollView();
+            GUILayout.BeginHorizontal();
+            Texture2D tex = UnityEditorInternal.InternalEditorUtility.GetIconForFile("Assets/Editor/MyCustomSettings.asset");
+            GUILayout.Label(new GUIContent(tex), GUILayout.Width(256), GUILayout.Height(256));
+            //Texture2D tex2 = UnityEditorInternal.InternalEditorUtility.GetIconForFile("Assets/Prefabs/BigSphere.prefab");
+            //GUILayout.Label(new GUIContent(tex2), GUILayout.Width(256), GUILayout.Height(256));
+            GUILayout.EndHorizontal();
+        }
+        else
+        {
+
+            List<Match> matches = guids.Cast<Match>().ToList();
+            List<string> matchPaths = matches.Select(match => AssetDatabase.GUIDToAssetPath(match.Value)).ToList();
+
+            int largestBox = matchPaths.OrderByDescending(pth => pth.Length).First().Length;
+
+            scrollPos = GUILayout.BeginScrollView(scrollPos, GUILayout.Width(192 + largestBox * editButtonSizeMod + 4));
+            
+            foreach (Match xmatch in guids)
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.Label(new GUIContent(xmatch.Value), GUILayout.Width(160));
+
+                GUILayout.Label(new GUIContent("->"), GUILayout.Width(32));
+                string assetPath = AssetDatabase.GUIDToAssetPath(xmatch.Value);
+
+                GUIStyle tmpStyle = new GUIStyle();
+                tmpStyle.border = new RectOffset(10,10,10,10);
+                tmpStyle.alignment = TextAnchor.MiddleLeft;
+
+                if (GUILayout.Button(new GUIContent(assetPath)))
+                {
+
+                }
+                GUILayout.EndHorizontal();
+            }
+            GUILayout.EndScrollView();
+        }
+    }
+}
