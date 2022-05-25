dafde8816d9d02005db5966f0a90d75208979687
Ronald <kpn.nl.rs@gmail.com>
2022-05-25 11:03:20 +0200

    Add performance mode for large scenes

--- Assets/Scripts/Editor/SceneReferenceEditor.cs
+++ Assets/Scripts/Editor/SceneReferenceEditor.cs
@@ -13,14 +13,16 @@ public class SceneReferenceEditor : EditorWindow
     public static string RegexExpression { get; }  = "([0-9a-f]){32}";
     public static RegexOptions regexOptions { get; } = RegexOptions.Multiline;
 
-    public int MaxRows = 3000;
+    public int MaxRows = 1000;
     public int editButtonSizeMod = 8;
     public string ScorePath;
 
     Vector2 scrollPos = Vector2.zero;
     MatchCollection guids;
+    HashSet<Match> guidsNoDupe;
     bool editMode = false;
     bool reload = true;
+    bool performance = false;
     GUIStyle overMaxStyle = new GUIStyle();
 
     [MenuItem("Tools/Experimental/View scene references")]
@@ -28,7 +30,12 @@ public class SceneReferenceEditor : EditorWindow
     {
         SceneReferenceEditor window = GetWindow<SceneReferenceEditor>("Reference editor [View]");
         window.ScorePath = EditorSceneManager.GetActiveScene().path;
+        window.guidsNoDupe = new HashSet<Match>();
         window.guids = Regex.Matches(File.ReadAllText(window.ScorePath), RegexExpression);
+        for (int i = 0; i < window.guids.Count; i++)
+        {
+            window.guidsNoDupe.Add(window.guids[i]);
+        }
         window.reload = false;
     }
     [MenuItem("Tools/Experimental/Edit scene references")]
@@ -37,13 +44,19 @@ public class SceneReferenceEditor : EditorWindow
         SceneReferenceEditor window = GetWindow<SceneReferenceEditor>("Reference editor [Edit]");
         window.ScorePath = EditorSceneManager.GetActiveScene().path;
         window.guids = Regex.Matches(File.ReadAllText(window.ScorePath), RegexExpression, regexOptions);
+        for (int i = 0; i < window.guids.Count; i++)
+        {
+            window.guidsNoDupe.Add(window.guids[i]);
+        }
         window.editMode = true;
         window.reload = false;
     }
     private void OnEnable()
     {
+        if (!reload)
+            Debug.Log("[SceneReferenceEditor] Reloading...");
         reload = true;
-        Debug.Log("[SceneReferenceEditor] Reloading...");
+
     }
 
     private void OnGUI()
@@ -52,6 +65,11 @@ public class SceneReferenceEditor : EditorWindow
         {
             ScorePath = EditorSceneManager.GetActiveScene().path;
             guids = Regex.Matches(File.ReadAllText(ScorePath), RegexExpression, regexOptions);
+            guidsNoDupe = new HashSet<Match>();
+            for (int i = 0; i < guids.Count; i++)
+            {
+                guidsNoDupe.Add(guids[i]);
+            }
             reload = false;
             Debug.Log("[SceneReferenceEditor] Reloaded!");
         }
@@ -59,16 +77,17 @@ public class SceneReferenceEditor : EditorWindow
         if (!editMode)
         {
             scrollPos = GUILayout.BeginScrollView(scrollPos);
-            //foreach (Match xmatch in guids)
-            for (int i = 0; i < guids.Count; i++)
+            int i = 0;
+            foreach (Match xmatch in guidsNoDupe)
+            //for (int i = 0; i < guids.Count; i++)
             {
                 if (i >= MaxRows)
                 {
                     overMaxRows = true;
-                    break;
+                    if (performance) break;
                 }
-                    
-                Match xmatch = guids[i];
+
+                //Match xmatch = guidsNoDupe[i];
                 GUILayout.BeginHorizontal();
                 GUILayout.Label(new GUIContent(xmatch.Value), GUILayout.Width(184));
             
@@ -77,18 +96,34 @@ public class SceneReferenceEditor : EditorWindow
 
                 GUILayout.Label(new GUIContent(assetPath));
                 GUILayout.EndHorizontal();
+                i++;
             }
             GUILayout.EndScrollView();
+            Texture2D tex = UnityEditorInternal.InternalEditorUtility.GetIconForFile(ScorePath);
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(new GUIContent(tex), GUILayout.Width(32), GUILayout.Height(32));
             if (overMaxRows)
             {
                 overMaxStyle.fontStyle = FontStyle.Bold;
-                overMaxStyle.active.textColor = Color.red;
+                overMaxStyle.normal.textColor = Color.red;
+                overMaxStyle.alignment = TextAnchor.MiddleLeft;
                 //overMaxStyle.richText = true;
-                GUILayout.Label(new GUIContent($"{guids.Count}/{MaxRows}"), overMaxStyle);
+                GUILayout.Label(new GUIContent($"{guidsNoDupe.Count}/{MaxRows}"), overMaxStyle);
             }
-            GUILayout.BeginHorizontal();
-            Texture2D tex = UnityEditorInternal.InternalEditorUtility.GetIconForFile("Assets/Editor/MyCustomSettings.asset");
-            GUILayout.Label(new GUIContent(tex), GUILayout.Width(256), GUILayout.Height(256));
+            else
+            {
+                GUILayout.Label(new GUIContent($"{guidsNoDupe.Count}/{MaxRows}"));
+            }
+            performance = GUILayout.Toggle(performance, new GUIContent("Cap shown references"));
+            if (performance)
+            {
+                MaxRows = EditorGUILayout.IntField("Max references shown", MaxRows);
+            }
+            else 
+            { 
+                GUILayout.Box("                    ");
+            }
+            GUILayout.Label(new GUIContent("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t"));
             //Texture2D tex2 = UnityEditorInternal.InternalEditorUtility.GetIconForFile("Assets/Prefabs/BigSphere.prefab");
             //GUILayout.Label(new GUIContent(tex2), GUILayout.Width(256), GUILayout.Height(256));
             GUILayout.EndHorizontal();
