94517c75a06e14d1e4635631dccb28811c712848
Ronald <kpn.nl.rs@gmail.com>
2022-06-22 13:39:52 +0200

    Credits Anim

--- Assets/Scripts/UI/CreditsUI.cs
+++ Assets/Scripts/UI/CreditsUI.cs
@@ -1,13 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using DentedPixel;
+using Cinemachine;
 
 public class CreditsUI : MonoBehaviour
 {
-    public GameObject ToRotate;
-    public void Spin()
+    public float IntervalMoves = 0.3f;
+    public float MoveTime = 0.1f;
+
+    [SerializeField]
+    private GameObject ronaldThing;
+    [SerializeField]
+    private GameObject julianThing;
+    [SerializeField]
+    private GameObject chivanoThing;
+    [SerializeField]
+    private GameObject nigelThing;
+
+    [SerializeField] 
+    private CinemachinePath path;
+    [SerializeField]
+    private float spinAmount = 360f;
+    [SerializeField]
+    private float spinTime = 1f;
+
+    private void Awake()
     {
-        LeanTween.rotateAround(ToRotate, Vector3.forward, 60, 1f).setEase(LeanTweenType.easeInOutElastic);
+        //Fade Nigel
+        LeanTween.alphaCanvas(nigelThing.GetComponent<CanvasGroup>(), 0, 2.25f);
+        
+        //Move other team members so they are more centered
+        LeanTween.moveX(ronaldThing, ronaldThing.GetComponent<ABTrack>().End.x, MoveTime).setEaseOutExpo().setDelay(2.25f + IntervalMoves * 2);
+        LeanTween.moveX(julianThing, julianThing.GetComponent<ABTrack>().End.x, MoveTime).setEaseOutExpo().setDelay(2.25f);
+        LeanTween.moveX(chivanoThing, chivanoThing.GetComponent<ABTrack>().End.x, MoveTime).setEaseOutExpo().setDelay(2.25f + IntervalMoves);
+
+        LTSpline spline = new LTSpline(path.m_Waypoints.Select<CinemachinePath.Waypoint, Vector3>(current =>
+        {
+            return path.transform.position + current.position;
+        }).ToArray()) ;
+
+        //LeanTween.move(ronaldThing, spline, 3f).setDelay(2.25f + IntervalMoves * 3).setEaseOutExpo();
+    }
+
+    public void Spin(GameObject go)
+    {
+        LeanTween.rotateAround(go, Vector3.forward, spinAmount, spinTime);
     }
 }
